f7d67e48a4e173c982d70db435396c1781fea7fe
syboosyboo <john782@ajou.ac.kr>
2022-10-24 11:20:34 +0900

    기능 추가 : 개발환경 셋팅

---
+++ comproject-server/src/main/java/com/sorhive/main/controller/MainController.java
@@ -0,0 +1,36 @@
+package com.sorhive.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * <pre>
+ * Class : MainController
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-10-18       부시연           최초 생성
+ * </pre>
+ *
+ * @author 부시연(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+@Controller
+public class MainController {
+
+    @GetMapping(value = {"/", "/main"})
+    public String main() {
+
+        return "main/main";
+
+    }
+
+    @PostMapping(value="/")
+    public String redirectMain() {
+
+        return "redirect:/";
+    }
+}
---
+++ comproject-server/src/test/java/com/sorhive/comproject/ComprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorhive.comproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
