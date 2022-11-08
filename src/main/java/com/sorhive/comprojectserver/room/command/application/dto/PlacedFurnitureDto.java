eeecc0ae247efd29c41cae564423952360519e74
syboosyboo <john782@ajou.ac.kr>
2022-11-08 11:53:07 +0900

    기능 추가 : 방 생성 전송객체 추가

---
+++ src/main/java/com/sorhive/comprojectserver/room/command/application/dto/PlacedFurnitureDto.java
@@ -0,0 +1,62 @@
+package com.sorhive.comprojectserver.room.command.application.dto;
+
+/**
+ * <pre>
+ * Class : PlacedFurnitureDto
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-08       부시연           최초 생성
+ * </pre>
+ *
+ * @author 부시연(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+public class PlacedFurnitureDto {
+
+    private String furnitureCategoryNumber;
+    private Long furnitureNumber;
+
+    private AngleDto angle;
+    private BoxPositionDto boxPosition;
+    private PositionDto position;
+    private ScaleDto scale;
+
+    public PlacedFurnitureDto() {
+    }
+
+    public PlacedFurnitureDto(String furnitureCategoryNumber, Long furnitureNumber, AngleDto angle, BoxPositionDto boxPosition, PositionDto position, ScaleDto scale) {
+        this.furnitureCategoryNumber = furnitureCategoryNumber;
+        this.furnitureNumber = furnitureNumber;
+        this.angle = angle;
+        this.boxPosition = boxPosition;
+        this.position = position;
+        this.scale = scale;
+    }
+
+    public String getFurnitureCategoryNumber() {
+        return furnitureCategoryNumber;
+    }
+
+    public Long getFurnitureNumber() {
+        return furnitureNumber;
+    }
+
+    public AngleDto getAngle() {
+        return angle;
+    }
+
+    public BoxPositionDto getBoxPosition() {
+        return boxPosition;
+    }
+
+    public PositionDto getPosition() {
+        return position;
+    }
+
+    public ScaleDto getScale() {
+        return scale;
+    }
+}
