fec4e58149d3dc2f078073417e64836669a5730c
syboosyboo <john782@ajou.ac.kr>
2022-11-10 12:29:21 +0900

    기능 변경 : 유니티에서 파일을 보낼 때 바이트 배열로 보내야 해서 그에 맞게 수정

--- src/main/java/com/sorhive/comprojectserver/room/command/application/controller/RoomController.java
+++ src/main/java/com/sorhive/comprojectserver/room/command/application/controller/RoomController.java
@@ -4,10 +4,10 @@ import com.sorhive.comprojectserver.common.ResponseDto;
 import com.sorhive.comprojectserver.room.command.application.dto.RoomCreateDto;
 import com.sorhive.comprojectserver.room.command.application.service.RoomService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
 
 /**
  * <pre>
@@ -19,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
  * ----------------------------------------------------------------
  * 2022-11-03       부시연           최초 생성
  * 2022-11-09       부시연           방 생성 추가
+ * 2022-11-10       부시연           접속용 방이미지 생성 추가
  * </pre>
  *
  * @author 부시연(최초 작성자)
@@ -34,14 +35,11 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    @PostMapping(value = "room", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<ResponseDto> createRoom(@RequestHeader String Authorization,
-                                                  @RequestPart(name="furnitures") RoomCreateDto roomCreateDto,
-                                                  @RequestPart(name="onlineRoomImage") MultipartFile onlineRoomImage,
-                                                  @RequestPart(name = "offlineRoomImage") MultipartFile offlineRoomImage) {
+    @PostMapping(value = "room")
+    public ResponseEntity<ResponseDto> createRoom(@RequestHeader String Authorization, @Valid @RequestBody RoomCreateDto roomCreateDto) {
 
         String accessToken = Authorization.substring(7);
 
-        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.CREATED, "방 생성 성공", roomService.createRoom(accessToken, roomCreateDto, onlineRoomImage, offlineRoomImage)));
+        return ResponseEntity.ok().body(new ResponseDto(HttpStatus.CREATED, "방 생성 성공", roomService.createRoom(accessToken, roomCreateDto)));
     }
 }
