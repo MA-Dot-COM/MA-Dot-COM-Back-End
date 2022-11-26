6dfd6c7129e84e7eba2e57e04ef2d2e5d5af7061
syboosyboo <john782@ajou.ac.kr>
2022-11-26 15:14:45 +0900

    버그 수정 : 회원 조회 시 MEMBER 역할 가진 사람만 조회, 회원 상세 조회시 삭제 된 회원 조회 수정

---
+++ src/main/java/com/sorhive/comprojectserver/member/query/mongorecommend/MongoRecommendData.java
@@ -0,0 +1,40 @@
+package com.sorhive.comprojectserver.member.query.mongorecommend;
+
+import lombok.Getter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * <pre>
+ * Class : MongoRecommend
+ * Comment: 회원 추천 조회용 도큐먼트
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-24       부시연           최초 생성
+ * </pre>
+ *
+ * @author 부시연(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see (참고할 class 또는 외부 url)
+ */
+@Getter
+@Document(collection = "recommend")
+public class MongoRecommendData {
+
+    @Id
+    private String id;
+    private List<String> memberCodes;
+    private List<List<String>> guestCodes;
+    private LocalDateTime uploadTime;
+
+    public MongoRecommendData(List<String> memberCodes, List<List<String>> guestCodes) {
+        this.memberCodes = memberCodes;
+        this.guestCodes = guestCodes;
+        this.uploadTime = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/sorhive/comprojectserver/member/query/mongorecommend/MongoRecommendQueryService.java
@@ -0,0 +1,27 @@
+package com.sorhive.comprojectserver.member.query.mongorecommend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * <pre>
+ * Class : MongoRecommendQueryService
+ * Comment: 회원 추천 조회용 서비스
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-25       부시연           최초 생성
+ * </pre>
+ *
+ * @author 부시연(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see (참고할 class 또는 외부 url)
+ */
+@Service
+public class MongoRecommendQueryService {
+
+    private static final Logger log = LoggerFactory.getLogger(MongoRecommendQueryService.class);
+
+}
