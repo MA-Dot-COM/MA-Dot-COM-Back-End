6d6529faf7d8ebbd51427598746bc46c0782acf7
syboosyboo <john782@ajou.ac.kr>
2022-12-10 17:44:07 +0900

    리팩토링 : 채팅 도메인 관련 예외 발생시 상태값과 메시지 추가

--- src/main/java/com/sorhive/comprojectserver/chatting/exception/NoMongoChattingException.java
+++ src/main/java/com/sorhive/comprojectserver/chatting/exception/NoMongoChattingException.java
@@ -1,4 +1,8 @@
 package com.sorhive.comprojectserver.chatting.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * <pre>
  * Class : NoMongoChattingException
@@ -8,12 +12,14 @@ package com.sorhive.comprojectserver.chatting.exception;
  * DATE             AUTHOR           NOTE
  * ----------------------------------------------------------------
  * 2022-11-19       부시연           최초 생성
+ * 2022-12-10       부시연           상태값 추가
  * </pre>
  *
  * @author 부시연(최초 작성자)
  * @version 1(클래스 버전)
  * @see (참고할 class 또는 외부 url)
  */
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "해당 채팅은 존재 하지 않습니다")
 public class NoMongoChattingException extends RuntimeException {
 
     public NoMongoChattingException() {
